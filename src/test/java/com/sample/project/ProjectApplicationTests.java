1198b568ec99291605fb0c60da71e51851714c09
Joseph Angelo M. Casauay <joseph.casauay@gmail.com>
2022-07-04 13:57:39 +0800

    test

--- src/test/java/com/sample/project/ProjectApplicationTests.java
+++ src/test/java/com/sample/project/ProjectApplicationTests.java
@@ -9,12 +9,12 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ProjectApplicationTests {
 
-	// @Test
-	// void testAPI(){
-	// 	String url = "http://localhost:3000/";
-	// 	RestTemplate restTemplate = new RestTemplate();
-	// 	String response = restTemplate.getForObject(url, String.class);
-	// 	assertEquals("Hello World", response);
-	// }
+	@Test
+	void testAPI(){
+		String url = "http://localhost:3000/";
+		RestTemplate restTemplate = new RestTemplate();
+		String response = restTemplate.getForObject(url, String.class);
+		assertEquals("Hello World", response);
+	}
 
 }
